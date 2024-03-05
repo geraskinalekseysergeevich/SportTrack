f72b9d3606c29d37e293a392c1641d62892d6c9b
owlahikit <owlahikit@gmail.com>
2024-03-05 22:33:03 +0300

    wowwow small bug in stats fixed

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,219 +2,256 @@ import React, { useState, useEffect } from 'react';
 import ProgressBar from 'react-customizable-progressbar';
 import { Chart } from 'react-google-charts';
 
-const StatisticsComponent = ({userId}) => {
+const StatisticsComponent = ({ userId }) => {
     const [exerciseData, setExerciseData] = useState([]);
     const [nutritionData, setNutritionData] = useState([]);
     const exerciseArray = [];
     const nutritionArray = [];
     const groupedData = [];
     const [inputData, setInputData] = useState({ items: [], exercises: [] });
-        
-    const recommendedTime = 60; 
-    const recommendedCalories = 2000;
 
+    const recommendedTime = 60;
+    const recommendedCalories = 2000;
 
     const dayToday = () => {
         var currentdate = new Date();
-        if(currentdate.getMonth()+ 1 < 10){
-        var datetime =
-            currentdate.getFullYear() +
-            '-0' +
-            (currentdate.getMonth() + 1) +
-            '-' +
-            currentdate.getDate();
-        } else datetime =
-        currentdate.getFullYear() +
-        '-' +
-        (currentdate.getMonth() + 1) +
-        '-' +
-        currentdate.getDate();
+        if (currentdate.getMonth() + 1 < 10) {
+            var datetime =
+                currentdate.getFullYear() +
+                '-0' +
+                (currentdate.getMonth() + 1) +
+                '-' +
+                currentdate.getDate();
+        } else
+            datetime =
+                currentdate.getFullYear() +
+                '-' +
+                (currentdate.getMonth() + 1) +
+                '-' +
+                currentdate.getDate();
         return datetime;
-    }
+    };
 
     const getPreviousWeek = () => {
         const currentDate = new Date();
         const currentDay = currentDate.getDate();
         const currentMonth = currentDate.getMonth() + 1;
         const currentYear = currentDate.getFullYear();
-      
         const weekArray = [];
-      
-        for (let i = 0; i < 7; i++) {
-          let date = new Date(currentYear, currentMonth - 1, currentDay - i);
-          let day = date.getDate();
-          let month = date.getMonth() + 1;
-          let year = date.getFullYear();
-      
-          if (month < 10) month = '0' + month;
-        
-          weekArray.push(`${year}-${month}-${day}`);
-        }
-      
-        return weekArray;
-      };
 
+        for (let i = 0; i < 7; i++) {
+            let date = new Date(currentYear, currentMonth - 1, currentDay - i);
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            let year = date.getFullYear();
+
+            if (month < 10) month = '0' + month;
+
+            weekArray.push(`${year}-${month}-${day}`);
+        }
+        return weekArray;
+    };
+
+    //пофиксить здесь вместо NaN выводить 0
     const getNutritionPercenatge = () => {
-        return nutritionData[dayToday()];
-    }
+        if (nutritionData[dayToday()] === undefined) return 0;
+        else return nutritionData[dayToday()];
+    };
 
     const getNutritionChartData = () => {
-        var ddata = [["Date", "Норма калорий, %"]];
-        var arr = nutritionData
+        var ddata = [['Date', 'Норма калорий, %']];
+        var arr = nutritionData;
         const weekArray = getPreviousWeek();
         for (var i = 0; i < weekArray.length; i++) {
             var date = weekArray[i];
-            var calories = arr[weekArray[i]];
+            if (arr[weekArray[i]] !== undefined) {
+                var calories = arr[weekArray[i]];
+            } else calories = 0;
             ddata.push([date, calories]);
-          }
-       return ddata;
-   }
+        }
+        return ddata;
+    };
 
-
-    const getExercisePercenatge = () =>{
-        return exerciseData[dayToday()];
-    }
+    const getExercisePercenatge = () => {
+        if (nutritionData[dayToday()] === undefined) return 0;
+        else return exerciseData[dayToday()];
+    };
 
     const getExerciseChartData = () => {
-        var ddata = [["Date", "Норма калорий, %"]];
-        var arr = exerciseData
+        var ddata = [['Date', 'Норма времени, %']];
+        var arr = exerciseData;
         const weekArray = getPreviousWeek();
         for (var i = 0; i < weekArray.length; i++) {
             var date = weekArray[i];
-            var calories = arr[weekArray[i]];
-            ddata.push([date, calories]);
-          }
-       return ddata;
-    }
+            if (arr[weekArray[i]] !== undefined) {
+                var time = arr[weekArray[i]];
+            } else time = 0;
+            ddata.push([date, time]);
+        }
+        return ddata;
+    };
 
-    
     useEffect(() => {
         const fetchData = async () => {
-            try{
-        const response = await fetch(`http://localhost:3001/api/users/user/data?userId=${userId}`)
-        const data = await response.json();
-        setInputData(data)
-        } catch (error){
-            console.log(error)
-        }
-    }
+            try {
+                const response = await fetch(
+                    `http://localhost:3001/api/users/user/data?userId=${userId}`
+                );
+                const data = await response.json();
+                setInputData(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
         fetchData();
     }, [userId]);
 
     useEffect(() => {
         const load = async () => {
-        inputData.items.forEach((item) => {
-                    let resultObject = {};
-                    if(item!=null && item!==undefined){
-                    item.forEach(el => {
-                        if(el!=null && el!==undefined){
+            inputData.items.forEach((item) => {
+                let resultObject = {};
+                if (item != null && item !== undefined) {
+                    item.forEach((el) => {
+                        if (el != null && el !== undefined) {
                             let [key, value] = el.split(':');
 
                             key = key.replace(/'/g, '').trim();
                             value = value.replace(/'/g, '').trim();
                             value = !isNaN(value) ? parseInt(value, 10) : value; // Проверяем, является ли значение числом
                             resultObject[key] = value;
-                    }
-                });
-                    groupedData.push(resultObject)
+                        }
+                    });
+                    groupedData.push(resultObject);
                 }
             });
             inputData.exercises.forEach((item) => {
                 let resultObject = {};
-                if(item!=null && item!==undefined){
-                item.forEach(el => {
-                    if(el!=null && el!==undefined){
-                        let [key, value] = el.split(':');
-                        key = key.replace(/'/g, '').trim();
-                        value = value.replace(/'/g, '').trim();
-                        value = !isNaN(value) ? parseInt(value, 10) : value; // Проверяем, является ли значение числом
-                        resultObject[key] = value;
+                if (item != null && item !== undefined) {
+                    item.forEach((el) => {
+                        if (el != null && el !== undefined) {
+                            let [key, value] = el.split(':');
+                            key = key.replace(/'/g, '').trim();
+                            value = value.replace(/'/g, '').trim();
+                            value = !isNaN(value) ? parseInt(value, 10) : value; // Проверяем, является ли значение числом
+                            resultObject[key] = value;
+                        }
+                    });
+                    groupedData.push(resultObject);
                 }
             });
-                groupedData.push(resultObject)
-            }
-        });
 
             groupedData.forEach((data) => {
-                console.log(data['time']=undefined);
-                if(data['time']!==undefined){
+                if (data['time'] !== undefined) {
                     exerciseArray.push({
                         date: data['date'],
+                        percentage: (data['time'] / recommendedTime) * 100,
+                    });
+                } else
+                    nutritionArray.push({
+                        date: data['date'],
                         percentage:
-                            (data['time'] / recommendedTime) * 100,
-                    })
-                }
-                else
-                nutritionArray.push({
-                    date: data['date'],
-                    percentage:
-                        (data['calories'] / recommendedCalories) * 100,
-                });
+                            (data['calories'] / recommendedCalories) * 100,
+                    });
             });
 
-            var groupedNutrition = []
-            nutritionArray.forEach(element => {
-                if(!(element['date'] in groupedNutrition))
+            var groupedNutrition = [];
+            nutritionArray.forEach((element) => {
+                if (!(element['date'] in groupedNutrition))
                     groupedNutrition[element['date']] = { percentage: 0 };
             });
             for (let item of nutritionArray) {
                 if (groupedNutrition[item.date]) {
                     groupedNutrition[item.date].percentage += item.percentage;
                 }
-              }
-            
-              var groupedExercises = []
-              exerciseArray.forEach(element => {
-                  if(!(element['date'] in groupedExercises))
-                  groupedExercises[element['date']] = { percentage: 0 };
-              });
-              for (let item of exerciseArray) {
-                  if (groupedExercises[item.date]) {
+            }
+
+            var groupedExercises = [];
+            exerciseArray.forEach((element) => {
+                if (!(element['date'] in groupedExercises))
+                    groupedExercises[element['date']] = { percentage: 0 };
+            });
+            for (let item of exerciseArray) {
+                if (groupedExercises[item.date]) {
                     groupedExercises[item.date].percentage += item.percentage;
-                  }
                 }
+            }
 
             const arr = [];
             const arr2 = [];
 
-            nutritionArray.forEach(obj => {
-            const date = obj.date;
-            if (!arr[date]) {
-                arr[date] = 0;
-            }
-            arr[date] += obj.percentage;
+            nutritionArray.forEach((obj) => {
+                const date = obj.date;
+                if (!arr[date]) {
+                    arr[date] = 0;
+                }
+                arr[date] += obj.percentage;
             });
 
-            exerciseArray.forEach(obj => {
+            exerciseArray.forEach((obj) => {
                 const date = obj.date;
                 if (!arr2[date]) {
                     arr2[date] = 0;
                 }
                 arr2[date] += obj.percentage;
-                });
-            
+            });
+
             setExerciseData(arr2);
             setNutritionData(arr);
-        }
+        };
         load();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [inputData.items]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [inputData.items]);
 
     return (
         <div>
-
-            <div style={{display:'inline-block'}}>
-            <div><h1>Нагрузки</h1><p>{parseInt(getExercisePercenatge())}%/100%</p></div>
-            <div style={{width:200, height:200, margin:'auto', display:'inline-block'}}>
-                <ProgressBar progress={getExercisePercenatge()} strokeColor='red'></ProgressBar>
+            <div style={{ display: 'inline-block' }}>
+                <div>
+                    <h1>Нагрузки</h1>
+                    <p>{parseInt(getExercisePercenatge())}%/100%</p>
+                </div>
+                <div
+                    style={{
+                        width: 200,
+                        height: 200,
+                        margin: 'auto',
+                        display: 'inline-block',
+                    }}
+                >
+                    <ProgressBar
+                        progress={getExercisePercenatge()}
+                        strokeColor="red"
+                    ></ProgressBar>
+                </div>
+                <div>
+                    <h1>Еда</h1>
+                    <p>{parseInt(getNutritionPercenatge())}%/100%</p>
+                </div>
+                <div
+                    style={{
+                        width: 200,
+                        height: 200,
+                        margin: 'auto',
+                        display: 'inline-block',
+                    }}
+                >
+                    <ProgressBar
+                        progress={getNutritionPercenatge()}
+                        strokeColor="lime"
+                    ></ProgressBar>
+                </div>
             </div>
-            <div><h1>Еда</h1><p>{parseInt(getNutritionPercenatge())}%/100%</p></div>
-            <div style={{width:200, height:200, margin:'auto', display:'inline-block'}}>
-                <ProgressBar progress={getNutritionPercenatge()} strokeColor='lime'></ProgressBar>
-            </div>
-            </div>
-            <Chart chartType="ColumnChart" width="100%" height="400px" data={getExerciseChartData()} />
-            <Chart chartType="ColumnChart" width="100%" height="400px" data={getNutritionChartData()} />
+            <Chart
+                chartType="ColumnChart"
+                width="100%"
+                height="400px"
+                data={getExerciseChartData()}
+            />
+            <Chart
+                chartType="ColumnChart"
+                width="100%"
+                height="400px"
+                data={getNutritionChartData()}
+            />
         </div>
     );
 };
