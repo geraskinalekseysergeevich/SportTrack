7f0a04a1db15879dfe5f6b5d534709231be89930
owlahikit <34578855+owlahikit@users.noreply.github.com>
2024-03-06 14:50:53 +0300

    Revert "Vova's pull request"

--- src/pages/TrainingsPage.jsx
+++ src/pages/TrainingsPage.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ExerciseForm from '../components/ExerciseForm';
 import { useLocation } from 'react-router-dom';
 
+
 const TrainingsPage = () => {
+    // Внутри вашего компонента
     const location = useLocation();
     // const { userId } = location.state;
     // console.log(userId);
